63029e8b7ca39794e764ef76c03d3a29e00c163d
Roy Chavarría <roychav99@gmail.com>
2019-10-23 19:32:42 -0600

    Copiado a la carpeta principal desde la interfaz exitoso

--- src/Controller.java
+++ src/Controller.java
@@ -5,7 +5,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.Button;
 import java.io.*;
 import java.text.SimpleDateFormat;
-
+import org.apache.commons.io.*;
 
 public class Controller {
     @FXML
@@ -17,55 +17,40 @@ public class Controller {
     @FXML
     private Button btn3;
 
-
     public void Agregar(ActionEvent actionEvent){
         FileChooser filechooser = new FileChooser();
         File select = filechooser.showOpenDialog(null);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-        File file = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files\\al.txt");
-        File dest = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files");
+        //SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 
         if(select != null){
-
             listView.getItems().add(select.getAbsolutePath());
-            System.out.println(sdf.format(file.lastModified()));
+            //System.out.println(sdf.format(file.lastModified()));
+            File source = select.getAbsoluteFile();
+            File dest = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files");
+            System.out.println(source);
+            System.out.println(dest);
+
+            try {
+                FileUtils.copyFileToDirectory(source, dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
        }else{
             System.out.println("Error: Invalid File");
        }
 
+
+
     }
 
     public void abrir() throws IOException {
-        String sour = listView.getSelectionModel().getSelectedItem().toString();
         FileReader read = new FileReader();
         String path = (String) listView.getSelectionModel().getSelectedItem();
         read.ReadFile(path);
 
-        File source = new File(sour);
-        File dest = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files\\dest.txt");
-        System.out.println(source);
-        System.out.println(dest);
-        copyFiles(source,dest);
-
     }
 
-    private static void copyFiles(File source, File dest) throws IOException {
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = new FileInputStream(source);
-            out = new FileOutputStream(dest);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
-            }
-        } finally {
-            in.close();
-            out.close();
-        }
-    }
 
     public void borrar(){
         int selected = listView.getSelectionModel().getSelectedIndex();
