c8df88ae23e7dfdde9b34882eded5af5719bd3d9
Roy Chavarría <roychav99@gmail.com>
2019-10-26 15:56:34 -0600

    Listo agregado de carpetas

--- src/Controller.java
+++ src/Controller.java
@@ -1,37 +1,44 @@
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Button;
 import java.io.*;
 import java.text.SimpleDateFormat;
+import javafx.stage.Stage;
 import org.apache.commons.io.*;
 
 public class Controller {
     @FXML
-    private Button btn1;
-    @FXML
     private ListView listView;
+    @FXML
+    private AnchorPane anchid;
+    @FXML
+    private Button btn1;
     @FXML
     private Button btn2;
     @FXML
     private Button btn3;
+    @FXML
+    private Button btn5;
 
-    public void Agregar(ActionEvent actionEvent){
+    public void archivo(){
         FileChooser filechooser = new FileChooser();
         File select = filechooser.showOpenDialog(null);
-        //SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 
         if(select != null){
             listView.getItems().add(select.getAbsolutePath());
-            //System.out.println(sdf.format(file.lastModified()));
             File source = select.getAbsoluteFile();
             File dest = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files");
+            System.out.println(sdf.format(dest.lastModified()));
             System.out.println(source);
             System.out.println(dest);
 
             try {
                 FileUtils.copyFileToDirectory(source, dest);
+                System.out.println(source.length());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -40,15 +47,39 @@ public class Controller {
             System.out.println("Error: Invalid File");
        }
 
+    }
 
+    public void carpeta(){
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Stage stage = (Stage) anchid.getScene().getWindow();
+        File select = directoryChooser.showDialog(stage);
+        File source = select.getAbsoluteFile();
+        File dest = new File("C:\\Users\\Dell\\Documents\\NetBeansProjects\\Proyecto2\\Files");
+
+        if(select != null){
+            File[] listOfFiles = select.listFiles();
+
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
+                    listView.getItems().add(file.getAbsolutePath());
+                    System.out.println(file.getName());
+                }
+            }
+
+            try {
+                FileUtils.copyDirectoryToDirectory(source, dest);
+                System.out.println(source.length());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
-    public void abrir() throws IOException {
+    public void abrir(){
         FileReader read = new FileReader();
         String path = (String) listView.getSelectionModel().getSelectedItem();
         read.ReadFile(path);
-
     }
 
 
